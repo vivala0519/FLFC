c821a99f3352ea038ae627d8f768c8d25729038d
Dzeko <vivala0519@gmail.com>
2024-03-17 23:27:34 +0900

    [feat] css 설정

--- src/components/LetsRecord.jsx
+++ src/components/LetsRecord.jsx
@@ -12,6 +12,12 @@ function LetsRecord() {
       {goal: ['영진', '승호', '지원'], assist: ['용병', '용병', '근한'], time: '8:45'},
       {goal: ['근한', '원효'], assist: ['영진', '근한'], time: '9:12'},
       {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
+      {goal: ['승호', '승호', '승호'], assist: [], time: '9:24'},
     ]
     setTodayRecord(dummy)
   }, []);
@@ -19,8 +25,8 @@ function LetsRecord() {
   return (
       <div className='flex flex-col'>
         {/*Today Record*/}
-        <span>{"Today's Record"}</span>
-        <div>
+        <span className='mt-3 mb-3'>{"Today's Record"}</span>
+        <div className='h-96 overflow-auto'>
           {
             todayRecord.map((record, index) =>
                 <div key={index}>
@@ -35,17 +41,18 @@ function LetsRecord() {
         )
           }
         </div>
+        <hr className='mt-5 mb-5' />
         {/*Write Container*/}
-        <span className='flex justify-center'>{round + 'R'}</span>
-        <div className='grid grid-cols-4 gap-3'>
-          <span className='flex justify-center'>골</span>
-          {scorer.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el}/>)}
+        <span className='flex justify-center mb-2'>{round + 'R'}</span>
+        <div className='flex mb-2 gap-2'>
+          <span className='flex justify-center' style={{width: '13%'}}>골</span>
+          {scorer.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
         </div>
-        <div className='grid grid-cols-4 gap-3'>
-          <span className='flex justify-center'>어시</span>
-          {assistant.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el}/>)}
+        <div className='flex gap-2'>
+          <span className='flex justify-center' style={{width: '13%'}}>어시</span>
+          {assistant.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
         </div>
-        <button>등록</button>
+        <button className='mt-5'>등록</button>
       </div>
   )
 }
