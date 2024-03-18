6a4638727f9245a414c52a4fc0e26ed30551ce3c
Dzeko <vivala0519@gmail.com>
2024-03-19 00:11:35 +0900

    [feat] css 설정

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
     }, []);
 
       return (
-          <div className='flex flex-col'>
+          <div className='flex flex-col items-center'>
               <header className='relative flex flex-col items-center w-full'>
-                  <span>FLFC</span>
+                  <span className='mb-5' style={{ letterSpacing: '3px', fontSize: '25px', fontFamily: 'Giants-Inline', fontStyle: 'normal', fontWeight: '400'}}>FLFC</span>
                   <div
-                      className='flex flex-row justify-evenly w-full border-solid border-2 border-indigo-600'>
+                      className='flex flex-row justify-around w-full border-solid border-2 border-indigo-600 mb-5 p-2' style={{ fontFamily: 'KBO-Dia-Gothic_bold'}}>
                       {tapName.map((tap, index) => <div key={index} onClick={() => setTap(index)}>{tap}</div>)}
                   </div>
               </header>
--- src/components/LetsRecord.jsx
+++ src/components/LetsRecord.jsx
@@ -5,6 +5,10 @@ function LetsRecord() {
   const [scorer, setScorer] = useState(['', '', ''])
   const [assistant, setAssistant] = useState(['', '', ''])
   const [todayRecord, setTodayRecord] = useState([])
+  const currentTime = new Date()
+    console.log(currentTime)
+    console.log(currentTime.getDay())
+    console.log(currentTime.getHours())
 
   useEffect(() => {
     const dummy = [
@@ -22,39 +26,50 @@ function LetsRecord() {
     setTodayRecord(dummy)
   }, []);
 
-  return (
-      <div className='flex flex-col'>
-        {/*Today Record*/}
-        <span className='mt-3 mb-3'>{"Today's Record"}</span>
-        <div className='h-96 overflow-auto'>
-          {
-            todayRecord.map((record, index) =>
-                <div key={index}>
-                  <span>{record.time}</span>
-                  <div className='grid grid-cols-4 gap-3'>
-                    <span className='flex justify-center'>골</span>
-                  </div>
-                  <div className='grid grid-cols-4 gap-3'>
-                    <span className='flex justify-center'>어시</span>
-                  </div>
+    return (
+        <>
+            <div className='flex flex-col items-center w-full'>
+                {/*Today Record*/}
+                <span className='mt-3 mb-1 underline underline-offset-1' style={{ fontFamily: 'Giants-Inline'}}>{"Today's Record"}</span>
+                <hr className='w-1/2 mb-5 border-indigo-600'/>
+                <div className='w-full h-96 overflow-auto'>
+                    {
+                        todayRecord.map((record, index) =>
+                            <div key={index} className='mb-2'>
+                                <span style={{ fontSize: '12px', fontFamily: 'Hahmlet'}}> {index + 1}R : {record.time}</span>
+                                <div className='flex mt-2 mb-2 gap-2 items-center'>
+                                    <span className='flex justify-center' style={{width: '13%', fontFamily: 'Giants-Inline'}}>골</span>
+                                    {record.goal.map((el, index) => <span key={index}
+                                                                          className='border-solid border-2 border-indigo-600'
+                                                                          style={{width: '26%', fontFamily: 'Hahmlet'}}>{el}</span>)}
+                                </div>
+                                <div className='flex gap-2 items-center'>
+                                    <span className='flex justify-center' style={{width: '13%', fontFamily: 'Giants-Inline'}}>어시</span>
+                                    {record.assist.map((el, index) => <span key={index}
+                                                                            className='border-solid border-2 border-indigo-600'
+                                                                            style={{width: '26%'}}>{el}</span>)}
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-        )
-          }
-        </div>
-        <hr className='mt-5 mb-5' />
-        {/*Write Container*/}
-        <span className='flex justify-center mb-2'>{round + 'R'}</span>
-        <div className='flex mb-2 gap-2'>
-          <span className='flex justify-center' style={{width: '13%'}}>골</span>
-          {scorer.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
-        </div>
-        <div className='flex gap-2'>
-          <span className='flex justify-center' style={{width: '13%'}}>어시</span>
-          {assistant.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
-        </div>
-        <button className='mt-5'>등록</button>
-      </div>
-  )
+                <hr className='w-1/2 mt-5 mb-5 border-indigo-600'/>
+                {/*Write Container*/}
+                <div className=''>
+                    <span className='flex justify-center mb-3'>{round + 'R'}</span>
+                    <div className='flex mb-2 gap-2 items-center'>
+                        <span className='flex justify-center' style={{width: '13%', fontFamily: 'Giants-Inline'}}>골</span>
+                        {scorer.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
+                    </div>
+                    <div className='flex gap-2 items-center'>
+                        <span className='flex justify-center' style={{width: '13%', fontFamily: 'Giants-Inline'}}>어시</span>
+                        {assistant.map((el, index) => <input className='border-solid border-2 border-indigo-600' key={index} value={el} style={{width: '26%'}}/>)}
+                    </div>
+                    <button className='mt-5'>등록</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LetsRecord
