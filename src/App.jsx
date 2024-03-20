3382aac4f7b6d01a858138e972a1b6dcb303f691
Dzeko <vivala0519@gmail.com>
2024-03-21 00:01:07 +0900

    [feat] 기록실 initialize

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import {useEffect, useState} from 'react'
 import LetsRecord from './components/LetsRecord.jsx'
 import RecordRoom from './components/RecordRoom.jsx'
 import WeeklyTeam from './components/WeeklyTeam.jsx'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { db } from '../firebase.js'
 import './App.css'
 
 function App() {
     const [tap, setTap] = useState(0)
     const tapName = ['기록하기', '기록실', '이번 주 팀']
+    const [data, setData] = useState([])
 
+    const dataGeneration = async () => {
+        // const dataSnapshot = doc(db, '2024', '0107')
+        // const response = await getDoc(dataSnapshot)
+        // console.log(response)
+        // console.log(response.data())
+        const collectionRef = collection(db, '2024');
+        const snapshot = await getDocs(collectionRef);
+        const fetchedData = snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }));
+        setData(fetchedData)
+    }
     useEffect(() => {
-
+        dataGeneration()
     }, []);
 
       return (
@@ -18,10 +31,10 @@ function App() {
                   <span className='mb-5' style={{ letterSpacing: '3px', fontSize: '25px', fontFamily: 'Giants-Inline', fontStyle: 'normal', fontWeight: '400'}}>FLFC</span>
                   <div
                       className='flex flex-row justify-around w-full border-solid border-0 border-b-2 border-t-2 border-indigo-600 mb-5 p-2' style={{ fontFamily: 'KBO-Dia-Gothic_bold'}}>
-                      {tapName.map((tap, index) => <div key={index} onClick={() => setTap(index)}>{tap}</div>)}
+                      {tapName.map((tap, index) => <div className='cursor-pointer' key={index} onClick={() => setTap(index)}>{tap}</div>)}
                   </div>
               </header>
-              {tap === 0 ? <LetsRecord/> : tap === 1 ? <RecordRoom/> : <WeeklyTeam/>}
+              {tap === 0 ? <LetsRecord/> : tap === 1 ? <RecordRoom data={data}/> : <WeeklyTeam/>}
               <footer></footer>
           </div>
       )
--- src/components/RecordRoom.jsx
+++ src/components/RecordRoom.jsx
@@ -1,11 +1,50 @@
 import {useEffect, useState} from 'react'
 
-function RecordRoom() {
+function RecordRoom(props) {
+    const {data} = props
+    const [tap, setTap] = useState(0)
+    const tapName = ['현황판', '출석', '골', '어시']
+    const [month, setMonth] = useState([])
 
-  return (
-      <div>
+    useEffect(() => {
+        console.log(data)
+        const monthSet = new Set()
+        data?.forEach(doc => {
+            monthSet.add(Number(doc.id.slice(0, 2)))
+        })
+        setMonth([...monthSet])
+    }, [])
+
+    useEffect(() => {
+        console.log(month)
+    }, [month])
+
+    return (
+      <div className='w-full'>
+            <div
+              className='flex flex-row justify-around w-full mb-5 p-2' style={{ fontFamily: 'Giants-Inline'}}>
+              {tapName.map((tap, index) => <div className='border-solid border-0 border-b-2 border-indigo-600 cursor-pointer text-sm' key={index} onClick={() => setTap(index)}>{tap}</div>)}
+          </div>
+          <div className='w-full'>
+              {/*{tap === 0 ? <div>현황판</div> : tap === 1 ? <div>출석</div> : tap === 2 ? <div>골</div> : <div>어시</div>}*/}
+              <table className='overscroll-x-auto' style={{width: '340px'}}>
+                  <thead>
+                      <tr>
+                          {month?.map((m, index) => (
+                              <th key={index} style={{width: '330px'}}>{m}</th>
+                          ))}
+                          {/*{calendarData.map((date, index) => (*/}
+                          {/*    <th key={index}>{date.getDate()}</th>*/}
+                          {/*))}*/}
+                      </tr>
+                  </thead>
+                  <tbody>
+                  {/* 이 부분에 나머지 테이블 내용을 추가할 수 있습니다. */}
+                  </tbody>
+              </table>
+          </div>
       </div>
-  )
+    )
 }
 
 export default RecordRoom
