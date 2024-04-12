8df9382eee077ff33fc8b37e1a149b4682307cbe
SHLee <seungho.lee@mytrum.com>
2024-04-12 21:40:28 +0900

    [feat] admin 페이지

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react"
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import {db} from "../../firebase.js"
+import styled from 'styled-components'
+
+const AdminPage = (props) => {
+  // const {  } = props
+
+  return (
+    <div>
+      Admin Page
+    </div>
+  )
+}
+
+export default AdminPage
