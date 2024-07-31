66497b3dd1f2f3353dcb8ade57fc46b78ca0dea7
Dzeko <vivala0519@gmail.com>
2024-08-01 07:51:46 +0900

    [feat] vote router, loading flag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from '@/components/pages/MainPage.jsx'
-import VotingPage from '@/components/pages/VotingPage.jsx'
+// import VotingPage from '@/components/pages/VotingPage.jsx'
 import AdminPage from '@/components/pages/AdminPage.jsx'
 import updateCurrentTime from '@/hooks/updateCurrentTime.js'
 import updateRecords from '@/hooks/updateRecords.js'
@@ -19,7 +19,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/test' element={<MainPage test={true} />} />
-        <Route path='/vote' element={<VotingPage />} />
+        {/*<Route path='/vote' element={<VotingPage />} />*/}
         <Route path='/admin' element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
