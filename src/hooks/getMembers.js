09ff6b785551fa9ff8d906b527cbbbdf80be2ee3
Dzeko <vivala0519@gmail.com>
2024-12-20 22:22:30 +0900

    [feat] member id atom, updateMembers 세팅

--- src/hooks/getMembers.js
+++ src/hooks/getMembers.js
@@ -1,11 +1,12 @@
 import { useAtom } from 'jotai'
-import { totalMembersAtom, existingMembersAtom } from '@/store/atoms'
+import { totalMembersAtom, existingMembersAtom, membersIdAtom } from '@/store/atoms'
 
 const getMembers = () => {
   const [totalMembers] = useAtom(totalMembersAtom)
   const [existingMembers] = useAtom(existingMembersAtom)
+  const [membersId] = useAtom(membersIdAtom)
 
-  return { totalMembers, existingMembers }
+  return { totalMembers, existingMembers, membersId }
 };
 
 export default getMembers
