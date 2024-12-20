ba0630f960b77164ad6b89587b90fd08644c6a13
Dzeko <vivala0519@gmail.com>
2024-12-20 22:28:39 +0900

    [fix] HistoryTap > css 조정

--- src/components/templates/ChildTap/HistoryTap.jsx
+++ src/components/templates/ChildTap/HistoryTap.jsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from "react"
-import {db} from "../../../../firebase.js"
-import {collection, getDocs} from "firebase/firestore"
+import { useEffect, useState } from 'react'
+import { db } from '../../../../firebase.js'
+import { collection, getDocs } from 'firebase/firestore'
 import styled from 'styled-components'
 import trophy from '@/assets/trophy.png'
 
@@ -10,7 +10,11 @@ const HistoryTap = () => {
   const getHistoryData = async () => {
     const historyRef = collection(db, 'history')
     const historySnapshot = await getDocs(historyRef)
-    const fetchedData = historySnapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })).filter(data => !['changed_last_season', 'last_season'].includes(data.id))
+    const fetchedData = historySnapshot.docs
+      .map((doc) => ({ id: doc.id, data: doc.data() }))
+      .filter(
+        (data) => !['changed_last_season', 'last_season'].includes(data.id),
+      )
     setHistoryData(fetchedData)
   }
 
@@ -19,42 +23,49 @@ const HistoryTap = () => {
   }, [])
 
   return (
-      <>
-      <div className='w-full flex justify-around mt-3 border-t-2 border-t-gray-200 pt-2 pb-2 border-b-2 border-b-gray-100'>
-        <Trophy style={{width: '40px'}}></Trophy>
-        <span className='flex items-center'>출석왕</span>
-        <span className='flex items-center'>득점왕</span>
-        <span className='flex items-center'>어시왕</span>
+    <>
+      <div className="w-full flex justify-around mt-3 border-t-2 border-t-gray-200 pt-2 pb-2 border-b-2 border-b-gray-200">
+        <Trophy style={{ width: '40px' }}></Trophy>
+        <span className="flex items-center">출석왕</span>
+        <span className="flex items-center">득점왕</span>
+        <span className="flex items-center">어시왕</span>
       </div>
-      {
-        historyData.map((data, index) => (
-            <div key={index} className='w-full flex justify-around mt-2 pb-2 items-center border-b-2 border-b-gray-100'>
-              <span style={{width: '40px'}}>{index === 0 ? '초' : index + 1}대</span>
-              <span className='flex flex-col'>{data.data['attendance_king'].map((name, idx) => <span key={idx}>{name}</span>)}</span>
-              <span>{data.data['goal_king']}</span>
-              <span>{data.data['assist_king']}</span>
-            </div>
-        ))
-      }
-      </>
+      {historyData.map((data, index) => (
+        <div
+          key={index}
+          className="w-full flex justify-around mt-2 pb-2 items-center border-b-2 border-b-gray-200"
+        >
+          <span style={{ width: '40px' }}>
+            {index === 0 ? '초' : index + 1}대
+          </span>
+          <span className="flex flex-col">
+            {data.data['attendance_king'].map((name, idx) => (
+              <span key={idx}>{name}</span>
+            ))}
+          </span>
+          <span>{data.data['goal_king']}</span>
+          <span>{data.data['assist_king']}</span>
+        </div>
+      ))}
+    </>
   )
 }
 
 export default HistoryTap
 
 const Trophy = styled.div`
-    position: relative;
-    width: 30px;
-    height: 30px;
-    &::after {
-        position: absolute;
-        content: '';
-        background-image: url(${trophy});
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        width: 77%;
-        height: 100%;
-        left: 20%;
-    }
+  position: relative;
+  width: 30px;
+  height: 30px;
+  &::after {
+    position: absolute;
+    content: '';
+    background-image: url(${trophy});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    width: 77%;
+    height: 100%;
+    left: 20%;
+  }
 `
