0fb93af62559939d11cf5a9114edd2a74f65e8fe
Dzeko <vivala0519@gmail.com>
2024-12-23 12:35:55 +0900

    [fix] 다크모드 설정 props로 조정

--- src/components/atoms/Text/TapTitleText.jsx
+++ src/components/atoms/Text/TapTitleText.jsx
@@ -1,9 +1,9 @@
 const TapTitleText = (props) => {
   const { active, title } = props
-  const titleStyle = 'relative bottom-1 mt-3 mb-1 underline underline-offset-1 font-dnf-forged' + (active ? ' text-[#EAB308]' : ' text-[#213547]')
-  return (
-    <span className={titleStyle}>{title}</span>
-  )
+  const titleStyle =
+    'relative bottom-1 mt-3 mb-1 underline underline-offset-1 font-dnf-forged' +
+    (active ? ' text-[#EAB308]' : ' text-[#213547] dark:text-gray-100')
+  return <span className={titleStyle}>{title}</span>
 }
 
 export default TapTitleText
