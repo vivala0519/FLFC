8e5519eb8506cdd9ad6e68cea24d18ac50e204e6
Dzeko <vivala0519@gmail.com>
2025-01-20 12:22:00 +0900

    [feat] adminPage > 3개월 이상 미참여자 추출

--- src/components/atoms/Text/RecordInput.jsx
+++ src/components/atoms/Text/RecordInput.jsx
@@ -1,13 +1,20 @@
 const RecordInput = (props) => {
-  const { type, setData } = props
-  const inputStyle = 'relative right-1 w-24 border-solid border-0 border-b-[1px] border-green-600 text-center outline-none z-1 bg-transparent'
+  const { type, setData, handleKeyDown } = props
+  const inputStyle =
+    'relative right-1 w-24 border-solid border-0 border-b-[1px] border-green-600 text-center outline-none z-1 bg-transparent'
 
   const onChangeHandler = (e) => {
     setData(e.target.value)
   }
 
   return (
-    <input className={inputStyle} value={type} onChange={onChangeHandler} maxLength={2}/>
+    <input
+      className={inputStyle}
+      value={type}
+      onChange={onChangeHandler}
+      onKeyDown={handleKeyDown}
+      maxLength={2}
+    />
   )
 }
 
--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -1,9 +1,10 @@
-import Swal from 'sweetalert2'
+// import Swal from 'sweetalert2'
 import styled from 'styled-components'
+import getMembers from '@/hooks/getMembers.js'
 
 import { uid } from 'uid'
 import { useEffect, useState } from 'react'
-import { doc, setDoc } from 'firebase/firestore'
+import { collection, doc, getDocs, setDoc } from 'firebase/firestore'
 import { getDatabase, ref, onValue, update, set } from 'firebase/database'
 
 import { db } from '../../../firebase.js'
@@ -11,6 +12,7 @@ import getTimes from '@/hooks/getTimes.js'
 
 const AdminPage = (props) => {
   const { recordData, weeklyTeamData } = props
+  const { existingMembers } = getMembers()
   const {
     time: { thisYear },
   } = getTimes()
@@ -18,49 +20,12 @@ const AdminPage = (props) => {
   const month = (currentTime.getMonth() + 1).toString().padStart(2, '0')
   const date = currentTime.getDate().toString().padStart(2, '0')
   const today = month + date
-  const players = [
-    '이승호',
-    '임준휘',
-    '우장식',
-    '이원효',
-    '김동휘',
-    '임희재',
-    '김규진',
-    '임건휘',
-    '한상태',
-    '노태훈',
-    '박근한',
-    '윤희철',
-    '정우진',
-    '홍원진',
-    '김남구',
-    '김민관',
-    '양대열',
-    '윤영진',
-    '임종우',
-    '황정민',
-    '손지원',
-    '방승진',
-    '전희종',
-    '황철민',
-    '선민조',
-    '최봉호',
-    '최수혁',
-    '김대건',
-    '김병일',
-    '김성록',
-    '박남호',
-    '선우용',
-    '윤준석',
-    '이재진',
-    '이진헌',
-    '장성민',
-    '전의준',
-    '진장용',
-    '하민수',
-    '황은집',
-  ]
 
+  const [startDate, setStartDate] = useState('')
+  const [totalFirestoreData, setTotalFirestoreData] = useState([])
+  const [threeMonthData, setThreeMonthData] = useState([])
+  const [threeMonthNonePlayer, setThreeMonthNonePlayer] = useState([])
+  const [threeMonthNonePlayerInfo, setThreeMonthNonePlayerInfo] = useState([])
   const [showTable, setShowTable] = useState(true)
   const [password, setPassword] = useState('')
   const [todayRecordObj, setTodayRecordObj] = useState({})
@@ -72,26 +37,26 @@ const AdminPage = (props) => {
   const [scorer, setScorer] = useState('')
   const [assistant, setAssistant] = useState('')
 
-  const passwordCheck = async () => {
-    const { value: password } = await Swal.fire({
-      title: '비밀번호 입력해주세요',
-      input: 'password',
-      inputPlaceholder: '********',
-    })
-    setPassword(password)
-  }
-  useEffect(() => {
-    passwordCheck()
-  }, [])
-
-  useEffect(() => {
-    if (password === '0413') {
-      setShowTable(true)
-    } else {
-      setPassword(null)
-      passwordCheck()
-    }
-  }, [password])
+  // const passwordCheck = async () => {
+  //   const { value: password } = await Swal.fire({
+  //     title: '비밀번호 입력해주세요',
+  //     input: 'password',
+  //     inputPlaceholder: '********',
+  //   })
+  //   setPassword(password)
+  // }
+  // useEffect(() => {
+  //   passwordCheck()
+  // }, [])
+  //
+  // useEffect(() => {
+  //   if (password === '0413') {
+  //     setShowTable(true)
+  //   } else {
+  //     setPassword(null)
+  //     passwordCheck()
+  //   }
+  // }, [password])
 
   useEffect(() => {
     if (showTable) {
@@ -297,11 +262,147 @@ const AdminPage = (props) => {
     setAssistant(e.target.value)
   }
 
+  useEffect(() => {
+    let resultArr = []
+    threeMonthNonePlayer.forEach((player) => {
+      let lastAttendanceDate = ''
+      totalFirestoreData.forEach((data) => {
+        if (Object.keys(data.data).includes(player)) {
+          lastAttendanceDate = data.id
+        }
+      })
+      resultArr.push([player, lastAttendanceDate])
+    })
+    resultArr = resultArr.sort((a, b) => {
+      const dateA = a[1] || '0'
+      const dateB = b[1] || '0'
+      return dateA.localeCompare(dateB)
+    })
+    console.log(resultArr)
+    setThreeMonthNonePlayerInfo(resultArr)
+  }, [threeMonthNonePlayer, totalFirestoreData])
+
+  const getLastSundayToTodayRange = () => {
+    const today = new Date()
+
+    // Step 1: 오늘이 일요일인지 확인
+    const isSunday = today.getDay() === 0 // 0 = Sunday
+
+    // Step 2: 오늘이 일요일이면 오늘 날짜 사용, 아니면 직전 일요일로 이동
+    const lastSunday = isSunday
+      ? today
+      : new Date(today.setDate(today.getDate() - today.getDay()))
+
+    // Step 3: 3개월 전 날짜 계산
+    const threeMonthsAgo = new Date(
+      lastSunday.getFullYear(),
+      lastSunday.getMonth() - 3,
+      lastSunday.getDate(),
+    )
+
+    return {
+      startDate: threeMonthsAgo,
+      endDate: lastSunday,
+    }
+  }
+
+  const getDataFunction = async (startDate) => {
+    // 전체 데이터 generate
+    const sinceYear = 2021
+    const dataList = []
+    for (let i = sinceYear; i < Number(thisYear) + 1; i++) {
+      const yearCollectionRef = collection(db, String(i))
+      const yearSnapshot = await getDocs(yearCollectionRef)
+      const fetchedYearData = yearSnapshot.docs
+        .map((doc) => ({
+          id: String(i).slice(2, 4) + doc.id,
+          data: doc.data(),
+        }))
+        .filter((data) => !data.id.includes('last_season_kings'))
+      fetchedYearData.forEach((data) => dataList.push(data))
+    }
+    setTotalFirestoreData(dataList)
+
+    // 3개월 데이터 추출
+    const startDateYear = String(startDate.getFullYear())
+    const collectionRef = collection(db, startDateYear)
+    const snapshot = await getDocs(collectionRef)
+    const fetchedData = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      data: doc.data(),
+    }))
+    let filteredIds = fetchedData.filter((data) => {
+      const idMonth = parseInt(data['id'].slice(0, 2), 10) - 1
+      const idDay = parseInt(data['id'].slice(2, 4), 10)
+
+      const idDate = new Date(Number(startDateYear), idMonth, idDay)
+
+      return idDate >= startDate
+    })
+
+    if (startDateYear !== thisYear) {
+      const thisYearCollectionRef = collection(db, thisYear)
+      const thisYearSnapshot = await getDocs(thisYearCollectionRef)
+      const fetchedThisYearData = thisYearSnapshot.docs
+        .map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+        .filter((data) => data.id !== 'last_season_kings')
+      filteredIds = filteredIds.concat(fetchedThisYearData)
+    }
+    setThreeMonthData(filteredIds)
+  }
+
+  useEffect(() => {
+    const players = []
+    threeMonthData?.forEach((data) => {
+      Object.keys(data['data']).forEach((name) => {
+        if (!players.includes(name)) {
+          players.push(name)
+        }
+      })
+    })
+    // console.log('existing : ', existingMembers)
+    // console.log(players)
+    const setMembers = new Set(existingMembers)
+    const setPlayers = new Set(players)
+    const difference = [...setMembers].filter((name) => !setPlayers.has(name))
+    setThreeMonthNonePlayer(difference)
+  }, [threeMonthData])
+
+  useEffect(() => {
+    const range = getLastSundayToTodayRange()
+    setStartDate(range.endDate.toISOString().slice(0, 10))
+
+    getDataFunction(range.startDate)
+  }, [])
+
   return (
     <div>
-      {showTable && (
+      <div>
+        <p>직전 일요일 ({startDate}) 기준</p>
+        <p>3개월 이상 미참여 인원 목록</p>
+        <table className="w-full mt-5">
+          <thead>
+            <tr>
+              <th style={{ padding: '8px' }}>이름</th>
+              <th style={{ padding: '8px' }}>마지막 참여일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {threeMonthNonePlayerInfo.map((data) => (
+              <tr key={data[0]}>
+                <td style={{ padding: '8px' }}>{data[0]}</td>
+                <td style={{ padding: '8px' }}>{data[1] ? data[1] : '-'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {resolvedList.length > 0 && requestList.length > 0 && (
         <div>
-          <span>요청 사항</span>
+          <span>기록 수정 요청 사항</span>
           <div className="border-2 border-amber-200 mt-2 h-32 overflow-y-auto p-1 flex flex-col gap-2">
             {resolvedList.map((request, index) => {
               return (
@@ -355,96 +456,96 @@ const AdminPage = (props) => {
               )
             })}
           </div>
-          <div
-            className={`flex items-center justify-center mt-5 ${!showRecordInput ? 'pl-12' : 'pl-0'}`}
-          >
-            <span>오늘의 기록</span>
-            {showRecordInput && (
-              <div className="flex ml-4 items-center">
-                <span className="w-8 text-xs flex items-center justify-center h-full">
-                  골 :
-                </span>
-                <input
-                  className="w-12 text-xs border-2 border-amber-100"
-                  style={{ textAlign: 'center' }}
-                  onChange={scorerHandler}
-                />
-                <span className="w-8 text-xs flex items-center justify-center h-full">
-                  어시 :
-                </span>
-                <input
-                  className="w-12 text-xs border-2 border-amber-100"
-                  style={{ textAlign: 'center' }}
-                  onChange={assistantHandler}
-                />
-              </div>
-            )}
-            {!showRecordInput ? (
-              <div
-                className="relative left-16 text-xs border-2 border-indigo-100 cursor-pointer"
-                onClick={() => setShowRecordInput(true)}
-              >
-                기록 추가하기
-              </div>
-            ) : (
-              <div
-                className="ml-2 text-xs border-2 border-indigo-100 cursor-pointer"
-                onClick={() => addRecordHandler()}
-              >
-                추가
-              </div>
-            )}
-          </div>
-          <RecordBox className="border-2 border-amber-200 mt-2 overflow-y-auto p-1">
-            <div>
-              {todayRecord.map((record, index) => {
-                return (
-                  <div key={index}>
-                    <div className="flex items-center mb-1 border-b-2 border-b-gray-200 h-10">
-                      <div className="flex flex-row justify-between items-center gap-3 w-full">
-                        <span className="text-xs">{record.time}</span>
-                        <div className="border-r-2 border-r-indigo-300 h-4"></div>
-                        <div className="flex">
-                          <span className="w-6 text-xs flex items-center justify-center h-full">
-                            골 :
-                          </span>
-                          <input
-                            className="w-12 text-xs border-2 border-amber-100"
-                            style={{ textAlign: 'center' }}
-                            value={record.goal}
-                            onChange={(e) =>
-                              goalInputChangeHandler(index, e.target.value)
-                            }
-                          />
-                        </div>
-                        <div className="border-r-2 border-r-indigo-300 h-4"></div>
-                        <div className="flex">
-                          <span className="w-10 text-xs flex items-center  justify-center h-full">
-                            어시 :{' '}
-                          </span>
-                          <input
-                            className="w-12 text-xs border-2 border-amber-100"
-                            style={{ textAlign: 'center' }}
-                            value={record.assist}
-                            onChange={(e) =>
-                              assistInputChangeHandler(index, e.target.value)
-                            }
-                          />
-                        </div>
-                        <div className="border-r-2 border-r-indigo-300 h-4"></div>
-                        <div
-                          className="text-xs flex items-center justify-center h-full w-12 border-2 border-indigo-100"
-                          onClick={() => updateRecordHandler(record.id)}
-                        >
-                          수정
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
-          </RecordBox>
+          {/*<div*/}
+          {/*  className={`flex items-center justify-center mt-5 ${!showRecordInput ? 'pl-12' : 'pl-0'}`}*/}
+          {/*>*/}
+          {/*  <span>오늘의 기록</span>*/}
+          {/*  {showRecordInput && (*/}
+          {/*    <div className="flex ml-4 items-center">*/}
+          {/*      <span className="w-8 text-xs flex items-center justify-center h-full">*/}
+          {/*        골 :*/}
+          {/*      </span>*/}
+          {/*      <input*/}
+          {/*        className="w-12 text-xs border-2 border-amber-100"*/}
+          {/*        style={{ textAlign: 'center' }}*/}
+          {/*        onChange={scorerHandler}*/}
+          {/*      />*/}
+          {/*      <span className="w-8 text-xs flex items-center justify-center h-full">*/}
+          {/*        어시 :*/}
+          {/*      </span>*/}
+          {/*      <input*/}
+          {/*        className="w-12 text-xs border-2 border-amber-100"*/}
+          {/*        style={{ textAlign: 'center' }}*/}
+          {/*        onChange={assistantHandler}*/}
+          {/*      />*/}
+          {/*    </div>*/}
+          {/*  )}*/}
+          {/*  {!showRecordInput ? (*/}
+          {/*    <div*/}
+          {/*      className="relative left-16 text-xs border-2 border-indigo-100 cursor-pointer"*/}
+          {/*      onClick={() => setShowRecordInput(true)}*/}
+          {/*    >*/}
+          {/*      기록 추가하기*/}
+          {/*    </div>*/}
+          {/*  ) : (*/}
+          {/*    <div*/}
+          {/*      className="ml-2 text-xs border-2 border-indigo-100 cursor-pointer"*/}
+          {/*      onClick={() => addRecordHandler()}*/}
+          {/*    >*/}
+          {/*      추가*/}
+          {/*    </div>*/}
+          {/*  )}*/}
+          {/*</div>*/}
+          {/*<RecordBox className="border-2 border-amber-200 mt-2 overflow-y-auto p-1">*/}
+          {/*  <div>*/}
+          {/*    {todayRecord.map((record, index) => {*/}
+          {/*      return (*/}
+          {/*        <div key={index}>*/}
+          {/*          <div className="flex items-center mb-1 border-b-2 border-b-gray-200 h-10">*/}
+          {/*            <div className="flex flex-row justify-between items-center gap-3 w-full">*/}
+          {/*              <span className="text-xs">{record.time}</span>*/}
+          {/*              <div className="border-r-2 border-r-indigo-300 h-4"></div>*/}
+          {/*              <div className="flex">*/}
+          {/*                <span className="w-6 text-xs flex items-center justify-center h-full">*/}
+          {/*                  골 :*/}
+          {/*                </span>*/}
+          {/*                <input*/}
+          {/*                  className="w-12 text-xs border-2 border-amber-100"*/}
+          {/*                  style={{ textAlign: 'center' }}*/}
+          {/*                  value={record.goal}*/}
+          {/*                  onChange={(e) =>*/}
+          {/*                    goalInputChangeHandler(index, e.target.value)*/}
+          {/*                  }*/}
+          {/*                />*/}
+          {/*              </div>*/}
+          {/*              <div className="border-r-2 border-r-indigo-300 h-4"></div>*/}
+          {/*              <div className="flex">*/}
+          {/*                <span className="w-10 text-xs flex items-center  justify-center h-full">*/}
+          {/*                  어시 :{' '}*/}
+          {/*                </span>*/}
+          {/*                <input*/}
+          {/*                  className="w-12 text-xs border-2 border-amber-100"*/}
+          {/*                  style={{ textAlign: 'center' }}*/}
+          {/*                  value={record.assist}*/}
+          {/*                  onChange={(e) =>*/}
+          {/*                    assistInputChangeHandler(index, e.target.value)*/}
+          {/*                  }*/}
+          {/*                />*/}
+          {/*              </div>*/}
+          {/*              <div className="border-r-2 border-r-indigo-300 h-4"></div>*/}
+          {/*              <div*/}
+          {/*                className="text-xs flex items-center justify-center h-full w-12 border-2 border-indigo-100"*/}
+          {/*                onClick={() => updateRecordHandler(record.id)}*/}
+          {/*              >*/}
+          {/*                수정*/}
+          {/*              </div>*/}
+          {/*            </div>*/}
+          {/*          </div>*/}
+          {/*        </div>*/}
+          {/*      )*/}
+          {/*    })}*/}
+          {/*  </div>*/}
+          {/*</RecordBox>*/}
         </div>
       )}
     </div>
