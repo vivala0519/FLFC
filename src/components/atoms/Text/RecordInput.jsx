560335f3106de628cc99bb158630f1b42d004c8a
Dzeko <vivala0519@gmail.com>
2025-03-14 16:41:49 +0900

    [fix] LetsRecord > WriteBox > input blur 시 typingStatus cleanup

--- src/components/atoms/Text/RecordInput.jsx
+++ src/components/atoms/Text/RecordInput.jsx
@@ -1,5 +1,5 @@
 const RecordInput = (props) => {
-  const { type, setData, handleKeyDown } = props
+  const { type, setData, handleKeyDown, handleBlur } = props
   const inputStyle =
     'relative right-1 w-24 border-solid border-0 border-b-[1px] border-green-600 text-center outline-none z-1 bg-transparent'
 
@@ -13,6 +13,7 @@ const RecordInput = (props) => {
       value={type}
       onChange={onChangeHandler}
       onKeyDown={handleKeyDown}
+      onBlur={handleBlur}
       maxLength={2}
     />
   )
